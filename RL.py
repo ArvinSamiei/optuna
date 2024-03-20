e066010f26bb48fe13a47c0df72740a23c460f91
ArvinSamiei <samiei.arvin@gmail.com>
2024-03-20 13:57:29 -0400

    add RL for 6DOF

--- RL.py
+++ RL.py
@@ -7,6 +7,8 @@ import numpy as np
 from gym import spaces
 from stable_baselines3 import PPO, A2C
 
+import utils
+from case_studies import CasesFacade
 from utils import run_iter_func
 
 step_size = 0.001
@@ -32,7 +34,10 @@ class AlgorithmClass:
     def __init__(self, alg):
         self.model_file_name = ''
         self.alg_class = None
-        self.env = gym.make('CollisionAvoidanceEnv-v0')
+        if utils.case_study == utils.CaseStudy.DOF6:
+            self.env = gym.make('CollisionAvoidanceEnv_6DOF-v0')
+        else:
+            self.env = gym.make('CollisionAvoidanceEnv-v0')
         if alg == RL_Algorithm.PPO:
             self.model_file_name = "ppo_cartpole"
             self.alg_class = PPO
@@ -195,11 +200,144 @@ class CollisionAvoidanceEnv(gym.Env):
             count += 1
 
 
+class CollisionAvoidanceEnv_6DOF(gym.Env):
+    """
+    Custom Environment for collision avoidance with high-dimensional state and action spaces.
+    """
+
+    def __init__(self):
+        super(CollisionAvoidanceEnv_6DOF, self).__init__()
+        # Define action and observation space
+        # Each state consists of 9 real numbers (positions of 3 objects)
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(6,), dtype=np.float32)
+
+        # Each action consists of 48 real numbers (motions for these objects)
+        self.action_space = spaces.Discrete(64)
+
+        self.cases_facade = CasesFacade()
+        self.function = self.cases_facade.run_iter_func
+
+        # Initialize state
+        self.reset_state()
+
+        self.max_reward = 0
+        self.max_reward_state = 0
+        self.max_reward_action = 0
+
+        # For early Stop
+        self.window_size = 100  # Size of the window for the moving average
+        self.improvement_threshold = 0.001  # Threshold for considering improvement significant
+        self.patience = 10  # How many checks without significant improvement to wait before stopping
+
+        # Initialize variables for tracking the moving average and stopping criteria
+        self.moving_averages = []
+        self.no_improvement_count = 0
+        self.total_rewards = 0
+        self.num_steps = 0
+        self.total_steps = 0
+
+    def step(self, action):
+        # Apply action
+        # Here you would need to define how an action modifies your environment's state
+
+        # Check for collision and calculate reward
+        bin_a = bin(action)
+        inputs = []
+        bin_a = bin_a[2:]
+        for b in bin_a:
+            if b == '0':
+                inputs.append(0.0)
+            else:
+                inputs.append(0.01)
+
+        if len(inputs) < 6:
+            for i in range(len(inputs), 6):
+                inputs.insert(0, 0)
+
+        reward = self.calculate_reward(inputs)
+
+        if reward <= 0:
+            return self.state, reward, True, {}
+
+        self.total_rewards += reward
+        self.num_steps += 1
+        self.total_steps += 1
+
+        if self.num_steps >= self.window_size:
+            moving_avg = self.total_rewards / self.num_steps
+            if len(self.moving_averages) >= 1:
+                improvement = moving_avg - self.moving_averages[-1]
+                if improvement < self.improvement_threshold:
+                    self.no_improvement_count += 1
+                r_history.append(moving_avg)
+            self.moving_averages.append(moving_avg)
+            self.no_improvement_count = 0
+            self.total_rewards = 0
+            self.num_steps = 0
+
+        done = False
+
+        if self.no_improvement_count >= self.patience:
+            done = True
+            print(f"Total steps were: {self.total_steps}")
+            self.no_improvement_count = 0
+            self.total_rewards = 0
+            self.num_steps = 0
+            self.total_steps = 0
+
+        # Check if the episode is done (you'll need to define the criteria)
+        if reward > self.max_reward:
+            self.max_reward = reward
+            self.max_reward_state = self.state.copy()  # Make a copy of the state
+            self.max_reward_action = action.copy()  # Make a copy of the action
+
+        for i in range(6):
+            self.state[i] += inputs[i]
+
+        return self.state, reward, done, {}
+
+    def reset(self, **kwargs):
+        # Reset the environment state
+        self.moving_averages = []
+        self.reset_state()
+        return self.state
+
+    def render(self, mode='human'):
+        # Render the environment to the screen (optional for your case)
+        pass
+
+    def close(self):
+        # Perform any necessary cleanup
+        pass
+
+    def calculate_reward(self, inputs):
+        # Define how to calculate the reward
+        # In your case, it's related to execution time for collision checking
+
+        execution_time = self.function([inputs])[0]
+        return execution_time / 30000000
+
+    def reset_state(self):
+        count = 0
+        while True:
+            self.state = self.observation_space.sample()
+            reward = self.calculate_reward([0] * 6)
+            if reward > 0:
+                print(f'count for resetting state was: {count}')
+                return
+            count += 1
+
+
 from gym.envs.registration import register
 
 register(
     id='CollisionAvoidanceEnv-v0',
     entry_point='RL:CollisionAvoidanceEnv',
+)
+
+register(
+    id='CollisionAvoidanceEnv_6DOF-v0',
+    entry_point='RL:CollisionAvoidanceEnv_6DOF',
 )
 
 rl_alg = AlgorithmClass(algorithm)
